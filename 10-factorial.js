507e2ae9374463c5b358480157f07eafabfbc1e7
your-github-username <your-email@example.com>
2025-09-14 13:12:14 +0300

    Task 11: recursively compute factorial of a number

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+const arg = parseInt(process.argv[2]);
+
+function factorial(n) {
+    if (isNaN(n) || n <= 0) {
+        return 1;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(arg));
